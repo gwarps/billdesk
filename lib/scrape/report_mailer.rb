5870e08748a647954c9d3c632b4a917d4d15f700
Puneet Singh <puneet@milaap.org>
2012-03-26 17:28:37 +0530

    Mailer Files Added

---
+++ lib/scrape/report_mailer.rb
@@ -0,0 +1,38 @@
+require "action_mailer"
+require "date"
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+ :address => "smtp.gmail.com",
+ :port => 587,
+ :domain => "gmail.com",
+ :user_name => "puneet@milaap.org",
+ :password => "puneet123",
+ :authentication => "plain",
+ :enable_starttls_auto => true 
+}
+
+ActionMailer::Base.view_paths = File.dirname(__FILE__)
+
+class UserMailer < ActionMailer::Base
+ default from: "puneet@milaap.org"
+
+ def send_mail(from_date,to_date,total,saved,failed,conflict)
+  @sub = "Billbharo Summary Report"
+  @f_date =  from_date
+  @t_date = to_date
+  @total = total
+  @saved = saved
+  @failed = failed
+  @conflict = conflict
+
+  if (to_date-from_date) ==  6
+   @sub = "Billbharo::Status check for last 7 days"
+  end
+
+  mail(:to => "puneet.mir@gmail.com",:subject => @sub) do |format|
+   format.html
+  end
+ end
+end
